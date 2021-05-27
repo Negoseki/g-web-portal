46f8c58ed731c2eab48fb10fe239cad74ecd9530
Jorge Nunes <Jorge.Silva-consultorexterno@rabobank.com>
2021-05-27 16:24:10 -0300

    subindo componente loading

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { MaterialModule } from '@gal/shared/material.module';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoadingComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [LoadingComponent],
+  entryComponents: [LoadingComponent]
 })
 export class CoreModule { }
